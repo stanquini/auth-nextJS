e7f4689ffd2428d2578e6ad0440cd1b1d99d0df5
Alfredo Stanquini Neto <stanquini@gmail.com>
2022-04-13 22:02:51 -0300

    add contexto + axios + save token in cookies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import styles from '../styles/Home.module.css'
+import { FormEvent, useContext, useState } from 'react';
+import { AuthContext } from '../styles/contexts/AuthContext';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    }
+    await signIn(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <p>Hello World</p>
-    </div>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
---
+++ styles/contexts/AuthContext.tsx
@@ -0,0 +1,65 @@
+import Router from 'next/router';
+import { setCookie } from 'nookies';
+import { createContext, ReactNode, useState } from "react";
+import { api } from "../../services/api";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+type User = {
+  email: string,
+  permissions: string[];
+  roles: string[];
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      })
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/',
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/',
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
+      Router.push('/dashboard')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
